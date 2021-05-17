53c931d945079bd096803b43a02ac4ca2b36b035
limnax <gitz45@gmail.com>
2021-05-17 13:53:29 +0300

    change image scale

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function renderPlaces(places) {
         model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.5 0.5 0.5');
+        model.setAttribute('scale', '0.2 0.2 0.2');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
