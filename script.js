4697ad9a2fb2a13e9c75a336b67616a878718208
limnax <gitz45@gmail.com>
2021-05-19 10:36:03 +0300

    image tracking

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: -26.004539,
+                lng: 28.133410,
             },
         },
     ];
