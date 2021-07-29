09c194ca34dfa65130dc3ccabcb19a810e34f098
limnax <gitz45@gmail.com>
2021-07-29 13:51:37 +0300

    add a table model

--- examples/nft_improved_worker/threejs_changed_3d.js
+++ examples/nft_improved_worker/threejs_changed_3d.js
@@ -119,7 +119,7 @@ function start( container, marker, video, btn, input_width, input_height, canvas
     /* Load Model */
     var threeGLTFLoader = new THREE.GLTFLoader();
 
-    threeGLTFLoader.load("../Data/models/Flamingo.glb", function (gltf) {
+    threeGLTFLoader.load("../Data/models/table.glb", function (gltf) {
             model = gltf.scene.children[0];
             model.position.z = 0;
             model.position.x = 100;
