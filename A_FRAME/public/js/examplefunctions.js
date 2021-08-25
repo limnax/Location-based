516dbd069ba400ca985dfdd80f3e8154bc7bfdae
limnax <gitz45@gmail.com>
2021-08-25 14:07:40 +0300

    A-Frame

---
+++ A_FRAME/public/js/examplefunctions.js
@@ -0,0 +1,7 @@
+// Simple example function:
+let addingfunction = (x, y) => {
+  return x + y;   // The function returns the product of x and y
+}
+
+console.log(addingfunction(3, 5)); //console will log 8
+
